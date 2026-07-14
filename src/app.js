a07c62cc5628e10cfbb643065bf4e6a622bf5eee
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-14 09:57:43 -0400

    Add client app entry

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import { renderRoute } from './router.js'
+
+const mount = () => {
+  document.querySelector('#app').innerHTML = renderRoute(location.pathname)
+  window.scrollTo(0, 0)
+}
+
+window.addEventListener('popstate', mount)
+window.addEventListener('click', event => {
+  const link = event.target.closest('a[data-route]')
+  if (!link) return
+  event.preventDefault()
+  history.pushState({}, '', link.getAttribute('href'))
+  mount()
+})
+
+mount()
