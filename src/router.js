8dffec931b53c79b5c0742f3ac8c69d1305ce34d
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-22 15:50:40 -0400

    feat: route partners landing

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import { quePage } from './pages/que.js'
 import { comoPage } from './pages/como.js'
+import { partnersPage } from './pages/partners.js'
 
 export const renderRoute = pathname => {
   if (pathname === '/como' || pathname === '/como/') return comoPage()
+  if (pathname === '/partners' || pathname === '/partners/') return partnersPage()
   return quePage()
 }
