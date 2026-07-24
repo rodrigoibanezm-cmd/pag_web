c8fcaefa9af2bc989c2fd29936102f27d345a842
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-23 22:54:35 -0400

    Route architecture page

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import { quePage } from './pages/que.js'
 import { comoPage } from './pages/como.js'
+import { arquitecturaPage } from './pages/arquitectura.js'
 import { partnersPage } from './pages/partners.js'
 
 export const renderRoute = pathname => {
   if (pathname === '/como' || pathname === '/como/') return comoPage()
+  if (pathname === '/arquitectura' || pathname === '/arquitectura/') return arquitecturaPage()
   if (pathname === '/partners' || pathname === '/partners/') return partnersPage()
   return quePage()
 }
