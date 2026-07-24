7624278a01b9432e94e24f3c07e6bfd0d72e7d5d
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-23 20:59:40 -0400

    Render architecture section on home

--- src/pages/que.js
+++ src/pages/que.js
@@ -2,11 +2,12 @@ import { nav, footer } from '../components/chrome.js'
 import { queHero } from '../components/que-hero.js'
 import { questionsSection } from '../components/questions.js'
 import { proof } from '../components/proof.js'
+import { architecture } from '../components/architecture.js'
 import { jobs } from '../components/jobs.js'
 import { systems } from '../components/systems.js'
 import { quePilot } from '../components/pilot.js'
 
 export const quePage = () => `
 ${nav('que')}
-<main id="top">${queHero()}${questionsSection()}${proof()}${jobs()}${systems()}${quePilot()}</main>
+<main id="top">${queHero()}${questionsSection()}${proof()}${architecture()}${jobs()}${systems()}${quePilot()}</main>
 ${footer()}`
