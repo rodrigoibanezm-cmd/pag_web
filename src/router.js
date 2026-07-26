feb56feabcf05381b1626e5baaf82233395ac3be
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-26 14:13:23 -0400

    Route /labs to long-form landing

--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import { quePage } from './pages/que.js'
 import { comoPage } from './pages/como.js'
 import { arquitecturaPage } from './pages/arquitectura.js'
 import { partnersPage } from './pages/partners.js'
+import { labsPage } from './pages/labs.js'
 
 export const renderRoute = pathname => {
   if (pathname === '/como' || pathname === '/como/') return comoPage()
   if (pathname === '/arquitectura' || pathname === '/arquitectura/') return arquitecturaPage()
   if (pathname === '/partners' || pathname === '/partners/') return partnersPage()
+  if (pathname === '/labs' || pathname === '/labs/') return labsPage()
   return quePage()
 }
