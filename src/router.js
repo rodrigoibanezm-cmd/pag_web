dca2a9d6b3aa5a1d5d8a672834976ad9de15c462
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-28 16:16:04 -0400

    Agrega ruta de experiencia de clientes

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { comoPage } from './pages/como.js'
 import { arquitecturaPage } from './pages/arquitectura.js'
 import { partnersPage } from './pages/partners.js'
 import { labsPage } from './pages/labs.js'
-import { ejemplosIndexPage, ejemplosPage, ejemplosErpPage, ejemplosCorreoPage } from './pages/ejemplos.js'
+import { ejemplosIndexPage, ejemplosPage, ejemplosErpPage, ejemplosCorreoPage, ejemplosClientesPage } from './pages/ejemplos.js'
 
 export const renderRoute = pathname => {
   if (pathname === '/como' || pathname === '/como/') return comoPage()
@@ -12,5 +12,6 @@ export const renderRoute = pathname => {
   if (pathname === '/ejemplos/crm' || pathname === '/ejemplos/crm/') return ejemplosPage()
   if (pathname === '/ejemplos/erp' || pathname === '/ejemplos/erp/') return ejemplosErpPage()
   if (pathname === '/ejemplos/correo' || pathname === '/ejemplos/correo/') return ejemplosCorreoPage()
+  if (pathname === '/ejemplos/clientes' || pathname === '/ejemplos/clientes/') return ejemplosClientesPage()
   return labsPage()
 }
