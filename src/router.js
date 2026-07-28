8a9e1eee8b5cfbe050573233f7aef6cd3b769767
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-28 14:48:58 -0400

    Route examples index separately

--- src/router.js
+++ src/router.js
@@ -2,12 +2,13 @@ import { comoPage } from './pages/como.js'
 import { arquitecturaPage } from './pages/arquitectura.js'
 import { partnersPage } from './pages/partners.js'
 import { labsPage } from './pages/labs.js'
-import { ejemplosPage } from './pages/ejemplos.js'
+import { ejemplosIndexPage, ejemplosPage } from './pages/ejemplos.js'
 
 export const renderRoute = pathname => {
   if (pathname === '/como' || pathname === '/como/') return comoPage()
   if (pathname === '/arquitectura' || pathname === '/arquitectura/') return arquitecturaPage()
   if (pathname === '/partners' || pathname === '/partners/') return partnersPage()
-  if (pathname === '/ejemplos' || pathname === '/ejemplos/' || pathname === '/ejemplos/crm' || pathname === '/ejemplos/crm/') return ejemplosPage()
+  if (pathname === '/ejemplos' || pathname === '/ejemplos/') return ejemplosIndexPage()
+  if (pathname === '/ejemplos/crm' || pathname === '/ejemplos/crm/') return ejemplosPage()
   return labsPage()
 }
