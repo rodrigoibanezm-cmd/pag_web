be0fd7e49b93669709d6b8a10c59828c62166c75
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-28 17:29:22 -0400

    Archive standalone architecture route

--- src/router.js
+++ src/router.js
@@ -1,12 +1,10 @@
 import { comoPage } from './pages/como.js'
-import { arquitecturaPage } from './pages/arquitectura.js'
 import { partnersPage } from './pages/partners.js'
 import { labsPage } from './pages/labs.js'
 import { ejemplosIndexPage, ejemplosPage, ejemplosErpPage, ejemplosCorreoPage, ejemplosClientesPage } from './pages/ejemplos.js'
 
 export const renderRoute = pathname => {
   if (pathname === '/como' || pathname === '/como/') return comoPage()
-  if (pathname === '/arquitectura' || pathname === '/arquitectura/') return arquitecturaPage()
   if (pathname === '/partners' || pathname === '/partners/') return partnersPage()
   if (pathname === '/ejemplos' || pathname === '/ejemplos/') return ejemplosIndexPage()
   if (pathname === '/ejemplos/crm' || pathname === '/ejemplos/crm/') return ejemplosPage()
