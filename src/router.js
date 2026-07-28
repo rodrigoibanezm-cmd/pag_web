6d7f8084d80a8cd87f92c365dc38d021de1aa907
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-28 14:59:00 -0400

    Route ERP example page

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import { comoPage } from './pages/como.js'
 import { arquitecturaPage } from './pages/arquitectura.js'
 import { partnersPage } from './pages/partners.js'
 import { labsPage } from './pages/labs.js'
-import { ejemplosIndexPage, ejemplosPage } from './pages/ejemplos.js'
+import { ejemplosIndexPage, ejemplosPage, ejemplosErpPage } from './pages/ejemplos.js'
 
 export const renderRoute = pathname => {
   if (pathname === '/como' || pathname === '/como/') return comoPage()
@@ -10,5 +10,6 @@ export const renderRoute = pathname => {
   if (pathname === '/partners' || pathname === '/partners/') return partnersPage()
   if (pathname === '/ejemplos' || pathname === '/ejemplos/') return ejemplosIndexPage()
   if (pathname === '/ejemplos/crm' || pathname === '/ejemplos/crm/') return ejemplosPage()
+  if (pathname === '/ejemplos/erp' || pathname === '/ejemplos/erp/') return ejemplosErpPage()
   return labsPage()
 }
