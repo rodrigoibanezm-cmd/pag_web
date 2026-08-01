d5ef7c7013a2e645a386e9ba48865dc37cff7de2
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-31 20:35:25 -0400

    Remove obsolete architecture page

--- src/pages/arquitectura.js
+++
@@ -1,7 +0,0 @@
-import { nav, footer } from '../components/chrome.js'
-import { architectureFaq } from '../components/architecture-faq.js'
-
-export const arquitecturaPage = () => `
-${nav('arquitectura')}
-<main id="top">${architectureFaq()}</main>
-${footer()}`
