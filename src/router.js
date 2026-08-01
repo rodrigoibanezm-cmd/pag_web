969965f9f1edbed9e354f1c8bfb8238253d661b2
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-31 20:34:00 -0400

    Remove obsolete page routes

--- src/router.js
+++ src/router.js
@@ -1,10 +1,8 @@
-import { comoPage } from './pages/como.js'
 import { partnersPage } from './pages/partners.js'
 import { labsPage } from './pages/labs.js'
 import { ejemplosIndexPage, ejemplosPage, ejemplosErpPage, ejemplosCorreoPage, ejemplosClientesPage } from './pages/ejemplos.js'
 
 export const renderRoute = pathname => {
-  if (pathname === '/como' || pathname === '/como/') return comoPage()
   if (pathname === '/partners' || pathname === '/partners/') return partnersPage()
   if (pathname === '/ejemplos' || pathname === '/ejemplos/') return ejemplosIndexPage()
   if (pathname === '/ejemplos/crm' || pathname === '/ejemplos/crm/') return ejemplosPage()
